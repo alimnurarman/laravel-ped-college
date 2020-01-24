a2105f7f2b3a66c1ac42abdc3d562ede612f58e0
Arman <you@abc.net>
2020-01-24 14:11:57 +0600

    0.1.7 old things anthem, emblem etcetera

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -66,6 +66,79 @@ return [
     'E-Mail Address' => 'Электронды пошта',
     'Password' => 'Құпия сөз',
     'Confirm Password' => 'Құпия сөзді растаңыз',
+
+
+    'calling_schedule' => 'CALLING SCHEDULE',
+    'schedule_time' => 'Schedule time',
+    'edu_work_1' => 'EDUCATIONAL AND EDUCATIONAL WORK',
+    'edu_work_2' => 'BEST PEDAGOGICAL COMPETITION',
+    'edu_work_3' => 'EDUCATIONAL WORK',
+    'japan' => 'JAPAN',
+    'edu_work_4' => 'INTERNATIONAL COOPERATION',
+
+    'qualification' => 'Qualification',
+    'basic_secondary_education' => 'Basic secondary education',
+    'elementary_education' => 'Elementary education',
+    'preschool_education_and_training' => 'Preschool education and training',
+    'kazakh_language_and_literature_teacher' => 'Kazakh language and literature teacher',
+    'teacher_of_russian_language_and_literature' => 'Teacher of Russian language and literature',
+    'high_school_foreign_language_teacher' => 'High School Foreign Language Teacher',
+    'computer_science_teacher_in_high_school' => 'Computer science teacher in high school',
+    'primary_education_teacher' => 'Primary education teacher',
+    'primary_education_teacher_with_english_education' => 'Primary education teacher with English education',
+    'translation_studies' => 'Translation Studies',
+    'preschool_education_and_training_2' => 'Preschool education and training',
+    'year_1978' => '1978 EDUCATIONAL PEDAGOGICAL SCHOOL IN ALMATY CITY SCHOOL',
+    'year_1990' => '1990 ALMATY PEDAGOGICAL SCHOOL # 1',
+    'year_1994_1' => '1994 ALMATY KAZAKHSTAN PEDAGOGICAL SCHOOL',
+    'year_1994_2' => '1994 THE COLLEGE STATUS HAS BEEN AWARDED',
+    'year_1999_1' => '1999 year SCIENTIFIC AND METHODICAL AREA OF THE REPUBLICAN PEDAGOGICAL COLLEGE',
+    'year_1999_2' => '1999 year . ALTYSARIN ACHIEVEMENT - SCIENTIFIC - METHODICAL EXPERIMENTAL SITE',
+    'year_1999_3' => '1999 year THE REPUBLICAN EDUCATION - METHODICAL COMMUNITY',
+    'year_2008' => '2008 ALMATY KAZAKHSTAN STATE HUMANITARIAN - PEDAGOGICAL COLLEGE',
+    'teachers_c_1' => 'Pedagogy, psychology and methods of individual disciplines members of the subject-methodical commission',
+    'teachers_c_2' => 'Chairman of the subject-methodical commission: Nurdaevletova ZN',
+    'teachers_c_3' => 'Members of the subject-methodical commission "Kazakh language and literature and teaching methods',
+    'teachers_c_4' => 'Chairman of the Disciplinary-methodical commission: YESENOVA E. T.',
+    'teachers_c_5' => 'ЖАЛПЫ ЖӘНЕ КӘСІПТІК БІЛІМ БЕРЕТІН ПӘНДЕР пәндік-әдістемелік комиссия мүшелері',
+    'teachers_c_6' => 'Chairman of the subject-methodical commission: Sadyrbekova AT',
+    'teachers_c_7' => 'History and socio-economic disciplines members of the subject-methodical commission',
+    'teachers_c_8' => 'Chairman of the Disciplinary-methodical commission: Bizhanov N.I.',
+    'teachers_c_9' => 'Foreign Languages ​​and Teaching Methods ',
+    'teachers_c_10' => 'Chairman of the subject-methodical commission: BEZHAZOVA M.Zh.',
+    'teachers_c_11' => 'Members of the subject-methodical commission PHYSICAL EDUCATION AND FIRST MILLION',
+    'our_a_c_1' => 'ACHIEVEMENTS OF SESSION',
+    'our_a_c_2' => 'ACHIEVEMENTS OF TEACHERS',
+    'our_a_c_3' => 'ACCESS STUDENTS',
+    'our_a_c_4' => 'ACCESS STUDENTS',
+
+    'public_procurement' => 'Public procurement',
+    'announcements' => 'Announcements',
+    'public_pro_c_1' => 'Contest for Catering Services Provider',
+    'annual_plans' => 'Annual plans',
+    'reports' => 'Reports',
+
+
+    'state_services' => 'State services',
+    'state_services_c_1' => 'DEPARTMENT OF EDUCATION OF THE CITY OF ALMATY',
+    'state_services_c_2' => 'ALMATY STATE PEDAGOGICAL COLLEGE',
+    'state_services_c_3' => 'ONLINE CATALOG',
+    'state_services_c_4' => 'LEGAL ACTS',
+    'state_services_c_5' => 'FOR ORGANIZATIONS OF TECHNICAL AND PROFESSIONAL EDUCATION OF THE CITY OF ALMATY',
+
+
+    'categories' => 'Categories',
+    'about_state_symbols' => 'About State Symbols of the Republic of Kazakhstan',
+    'about_state_symbols_f' => 'State Flag of the Republic of Kazakhstan',
+    'about_state_symbols_a' => 'National Anthem of the Republic of Kazakhstan',
+    'about_state_symbols_e' => 'State Emblem of the Republic of Kazakhstan',
+    'about_ss_flag' => 'STATE FLAG OF THE REPUBLIC OF KAZAKHSTAN',
+    'about_ss_emblem' => 'State Emblem of the Republic of Kazakhstan',
+    'about_ss_anthem' => 'State anthem of the Republic of Kazakhstan',
+
+
+
+
 ];
 
 ?>
